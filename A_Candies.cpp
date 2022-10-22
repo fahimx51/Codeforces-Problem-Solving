b4d3dadb0003b4c184a8ba3523614d7a08d5c6c1
Fahim <fahim69.dev@gmail.com>
2022-10-22 14:45:16 +0600

    Revert "Codeforces Soluation"
    This reverts commit e493604b231d439442f9cad33c2e60e97ca8c90e.

--- A_Candies.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    
-    cout<<pow(2,3);
-    
-    return 0;
-}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,2 @@
-int res1 = exist(x, v);
-        
 
-        // int res2 = exist(y, v);
-        // if(res2>=0) v.push_back(x);
-        // else v[res2] = x;
+    // cin.tie(NULL);
